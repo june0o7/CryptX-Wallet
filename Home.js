e065c0d1941bc9c40460d4a4873097749fdae6b1
Deepmala Haldar <halderdeepu114@gmail.com>
2024-10-29 21:30:12 +0530

    homepage modified, Dark Theme

--- Home.js
+++ Home.js
@@ -30,101 +30,109 @@ import { LinearGradient } from "expo-linear-gradient";
 function Home(props) {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "black", fontWeight: "bold", fontSize: 30 }}>
-        Hello, Alex !
-      </Text>
-      <Text style={{ color: "grey", fontSize: 15, marginTop: 10 }}>
-        Total Balance
-      </Text>
-
-      <Text style={{ color: "black", fontSize: 25, marginTop: 3 }}>
-        $10,000.00
-      </Text>
-      <Text
-        style={{
-          fontWeight: "bold",
-          padding: 2,
-          backgroundColor: "#e8e9eb",
-          borderRadius: 10,
-          width: 100,
-          height: 20,
-          alignItems: "center",
-          fontSize: 10,
-          color: "grey",
-        }}
-      >
-        Market Growth: 4.7%
-      </Text>
-
       <View
-        style={{
-          padding: 10,
-          marginTop: 10,
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-        }}
+        style={{ backgroundColor: "#FFDE21", borderRadius: 20, padding: 10 }}
       >
-        <TouchableOpacity style={styles.button}>
-          <Image
-            source={require("./assets/icons/uparrow.png")}
-            style={styles.image}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Image
-            source={require("./assets/icons/downarrow.png")}
-            style={styles.image}
-          />
-        </TouchableOpacity>
+        <Text style={{ color: "black", fontWeight: "bold", fontSize: 30 }}>
+          Hello, Alex !
+        </Text>
+        <Text style={{ color: "#2c2e2e", fontSize: 15, marginTop: 10 }}>
+          Total Balance
+        </Text>
 
-        <TouchableOpacity style={styles.button}>
-          <Image
-            source={require("./assets/icons/mg.png")}
-            style={styles.image}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.buttonDescription}>
-        <Text>Send </Text>
-        <Text>Receive </Text>
-        <Text>Find </Text>
+        <Text style={{ color: "black", fontSize: 25, marginTop: 3 }}>
+          $10,000.00
+        </Text>
+        <Text
+          style={{
+            fontWeight: "bold",
+            padding: 2,
+            backgroundColor: "#2c2e2e",
+            borderRadius: 10,
+            width: 110,
+            height: 20,
+            alignItems: "center",
+            fontSize: 10,
+            color: "#FFDE21",
+          }}
+        >
+          Market Growth: 4.7%
+        </Text>
+
+        <View
+          style={{
+            padding: 10,
+            marginTop: 10,
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={require("./assets/icons/uparrow.png")}
+              style={styles.image}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={require("./assets/icons/downarrow.png")}
+              style={styles.image}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={require("./assets/icons/mg.png")}
+              style={styles.image}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonDescription}>
+          <Text>Send </Text>
+          <Text>Receive </Text>
+          <Text>Find </Text>
+        </View>
       </View>
       <Text
         style={{
           fontWeight: "bold",
           padding: 5,
-          width: 70,
+          width: 90,
           alignItems: "center",
-          color: "#333436",
+          color: "white",
           fontSize: 18,
         }}
       >
-        Events
+        Popular
       </Text>
 
-      <View style={{ style: 200 }}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+      <View>
+        <ScrollView
+          style={{ backgroundColor: "#eda968", borderRadius: 20, padding: 4 }}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
           <TouchableOpacity style={{ margin: 10, height: 110 }}>
             <Image
-              source={require("./assets/icons/gg.png")}
+              source={require("./assets/images/eth.jpg")}
               style={{ width: 200, height: 100 }}
             />
           </TouchableOpacity>
           <TouchableOpacity style={{ margin: 10, height: 110 }}>
             <Image
-              source={require("./assets/icons/ff.png")}
+              source={require("./assets/images/dbi.jpg")}
               style={{ width: 200, height: 100 }}
             />
           </TouchableOpacity>
           <TouchableOpacity style={{ margin: 10, height: 110 }}>
             <Image
-              source={require("./assets/icons/kk.png")}
+              source={require("./assets/images/eth.jpg")}
               style={{ width: 200, height: 100 }}
             />
           </TouchableOpacity>
           <TouchableOpacity style={{ margin: 10, height: 110 }}>
             <Image
-              source={require("./assets/icons/jj.png")}
+              source={require("./assets/images/dbi.jpg")}
               style={{ width: 200, height: 100 }}
             />
           </TouchableOpacity>
@@ -137,119 +145,298 @@ function Home(props) {
           padding: 5,
           width: 120,
           alignItems: "center",
-          color: "#333436",
+          color: "white",
           fontSize: 18,
         }}
       >
         Businesses{" "}
       </Text>
-        <View>
-
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <TouchableOpacity>
+      <View
+        style={{
+          borderColor: "white",
+          borderWidth: 1,
+          borderRadius: 5,
+          padding: 5,
+        }}
+      >
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/jio-logo-icon.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/zomato.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/swiggy.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/bms.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/lenskart.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/jio-logo-icon.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/zomato.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/swiggy.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/bms.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-            <TouchableOpacity>
+          <TouchableOpacity>
             <Image
               source={require("./assets/icons/lenskart.png")}
-            style={{ width: 50, height: 50, margin:5 }}
+              style={{ width: 50, height: 50, margin: 5 }}
             />
-            </TouchableOpacity>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
 
-           
-          </ScrollView>
+      <View style={styles.transactions}>
+        <Text style={{ color: "white", fontWeight:"bold"}}>People</Text>
+
+        <View style={{ flexDirection: "row", flexWrap:"wrap", justifyContent:"space-evenly" }}>
+
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginLeft:2, marginRight:2}}>
+          <View style={{}}>
+            <Image
+              source={require("./assets/icons/profile.png")}
+              style={{ width: 50, height: 50, margin: 5 }}
+            />
+
+            <Text
+              style={{ color: "white", width: 60 }}
+              ellipsizeMode="tail"
+              numberOfLines={1}
+            >
+              Alex Hembrom
+            </Text>
+          </View>
+          </TouchableOpacity>
+
+          
         </View>
-
-      
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#2c2e2e",
     padding: 10,
   },
 
   button: {
-    backgroundColor: "white",
+    backgroundColor: "#FFDE21",
     width: 50,
     height: 50,
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
-    borderColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 0,
   },
   image: {
     borderRadius: 7,
-    width: 46,
-    height: 46,
+    width: 30,
+    height: 30,
     borderColor: "black",
   },
 
   buttonDescription: {
     flexDirection: "row",
     justifyContent: "space-evenly",
+  },
+
+  transactions: {
+    padding: 10,
   },
 });
 export default Home;
--- main.js
+++ main.js
@@ -1,32 +1,140 @@
-import { useEffect, useState } from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Home from './Home';
-import ID from './ID';
-import Wallet from './Wallet';
-import Set from './Set';
-import Ab from './Ab';
-import PayToContact from './PayToContact';
+import { useEffect, useState } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./Home";
+import ID from "./ID";
+import Wallet from "./Wallet";
+import Set from "./Set";
+import Ab from "./Ab";
+import PayToContact from "./PayToContact";
+import { Image, TouchableOpacity } from "react-native";
 
 function Main() {
+  const Drawer = createDrawerNavigator();
 
+  return (
+    <Drawer.Navigator initialRouteName="Home" 
+    screenOptions={{
 
-    const Drawer= createDrawerNavigator();
+        drawerStyle:{
+            backgroundColor:'#FFDE21',
+            
+        },
 
+       
 
-    return (
-
-        <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Wallet" component={Wallet} />
-            <Drawer.Screen name="Profile" component={ID} />
-            <Drawer.Screen name="Setting" component={Set} />
-            <Drawer.Screen name="About" component={Ab} />
-            <Drawer.Screen name="Pay To Contact" component={PayToContact} />
-        </Drawer.Navigator>
         
+
+        headerStyle:{
+            backgroundColor:'#2c2e2e',
+            shadowColor:'#2c2e2e'
+        
+        },
+        headerTintColor: 'white',
+           
       
-    
-    );
+        
+        headerRight: ()=>{
+
+            return(
+
+                <TouchableOpacity>
+                     <Image
+            source={require("./assets/icons/profile.png")}
+            style={{height:40, width:40, marginRight:10}}
+          />
+                </TouchableOpacity>
+            )
+
+
+        }
+    }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: () => {
+            return (
+              <Image
+                source={require("./assets/icons/home.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Wallet"
+        component={Wallet}
+        options={{
+          drawerIcon: ({ size }) => {
+            return (
+              <Image
+                source={require("./assets/icons/wallet.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={ID}
+        options={{
+          drawerIcon: ({ size }) => {
+            return (
+              <Image
+                source={require("./assets/icons/user.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Setting"
+        component={Set}
+        options={{
+          drawerIcon: ({ size }) => {
+            return (
+              <Image
+                source={require("./assets/icons/settings.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="About"
+        component={Ab}
+        options={{
+          drawerIcon: ({ size }) => {
+            return (
+              <Image
+                source={require("./assets/icons/about.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Pay To Contact"
+        component={PayToContact}
+        options={{
+          drawerIcon: ({ size }) => {
+            return (
+              <Image
+                source={require("./assets/icons/paytocontact.png")}
+                style={{ height: 20, width: 20 }}
+              />
+            );
+          },
+        }}
+      />
+    </Drawer.Navigator>
+  );
 }
 
 export default Main;
